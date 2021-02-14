091aca9610e66540402fbd60263c57f86acac6b0
tjamir <tjamir@gmail.com>
2021-02-13 21:16:23 -0300

    Adding example 1-1

---
+++ Chapter_1/1-1/printarea.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+double circularArea(double radius);
+
+int main(){
+
+	double radius = 1.0, area = 0.0;
+	printf ( "  Area of circles\n\n");
+	printf ( "   Radius         Area\n"
+		 "----------------------\n" );
+	area = circularArea(radius);
+	printf( "%10.1f     %10.2f\n", radius, area);
+
+
+	radius =5.0;
+	area = circularArea(radius);
+	printf( "%10.1f     %10.2f\n", radius, area);
+	
+	return 0;
+}	
+
+
+double circularArea(double radius){
+	return 3.14 * radius * radius;
+}
